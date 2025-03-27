522ce7334485d0a803d344a3c8b8a13822f887e0
Derpy <57187871+DerpDerpling@users.noreply.github.com>
2025-03-27 14:59:50 -0400

    Initial commit

---
+++ src/client/java/derp/animatedhotbar/AnimatedHotbarDataGenerator.java
@@ -0,0 +1,11 @@
+package derp.animatedhotbar;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AnimatedHotbarDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
